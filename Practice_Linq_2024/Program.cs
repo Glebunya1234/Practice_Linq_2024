fbb052b61c60bc2fbbbcb22d44a2de9014176484
Glebunya1234 <glebbuckiy@gmail.com>
2024-12-09 14:03:29 +0200

    Query1

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -11,7 +11,7 @@ namespace Practice_Linq_2024
             List<FootballGame> games = ReadFromFileJson(path);
 
             int test_count = games.Count();
-            Console.WriteLine($"Test value = {test_count}.");    // 13049
+            Console.WriteLine($"Test value = {test_count}."); // 13049
 
             Query1(games);
             Query2(games);
@@ -25,52 +25,45 @@ namespace Practice_Linq_2024
             Query10(games);
         }
 
-
         // Десеріалізація json-файлу у колекцію List<FootballGame>
         static List<FootballGame> ReadFromFileJson(string path)
         {
-
             var reader = new StreamReader(path);
             string jsondata = reader.ReadToEnd();
 
             List<FootballGame> games = JsonConvert.DeserializeObject<List<FootballGame>>(jsondata);
 
-
             return games;
-
         }
-
 
         // Запит 1
         static void Query1(List<FootballGame> games)
         {
             //Query 1: Вивести всі матчі, які відбулися в Україні у 2012 році.
 
-            var selectedGames = games; // Корегуємо запит !!!
-
+            var selectedGames = games.Where(g => g.Country == "Ukraine" && g.Date.Year == 2012); // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 1 ========================");
 
             // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+                Console.WriteLine(
+                    $"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}"
+                );
         }
 
         // Запит 2
         static void Query2(List<FootballGame> games)
         {
-            //Query 2: Вивести Friendly матчі збірної Італії, які вона провела з 2020 року.  
+            //Query 2: Вивести Friendly матчі збірної Італії, які вона провела з 2020 року.
 
             var selectedGames = games; // Корегуємо запит !!!
-
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 2 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 3
@@ -78,14 +71,12 @@ namespace Practice_Linq_2024
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 4
@@ -93,15 +84,12 @@ namespace Practice_Linq_2024
         {
             //Query 4: Вивести всі матчі збірної Германії з 2018 року по 2020 рік (включно), в яких вона на виїзді програла.
 
-            var selectedGames = games;   // Корегуємо запит !!!
-
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 4 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 5
@@ -110,15 +98,12 @@ namespace Practice_Linq_2024
             //Query 5: Вивести всі кваліфікаційні матчі (UEFA Euro qualification), які відбулися у Києві чи у Харкові, а також за умови перемоги української збірної.
 
 
-            var selectedGames = games;  // Корегуємо запит !!!
-
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 5 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 6
@@ -127,15 +112,12 @@ namespace Practice_Linq_2024
             //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
             //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
 
-            var selectedGames = games;   // Корегуємо запит !!!
-
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 7
@@ -143,15 +125,12 @@ namespace Practice_Linq_2024
         {
             //Query 7: Вивести перший матч у 2023 році, в якому збірна України виграла.
 
-            FootballGame g = null;   // Корегуємо запит !!!
-
+            FootballGame g = null; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 7 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 8
@@ -160,16 +139,13 @@ namespace Practice_Linq_2024
             //Query 8: Перетворити всі матчі Євро-2012 (UEFA Euro), які відбулися в Україні, на матчі з наступними властивостями:
             // MatchYear - рік матчу, Team1 - назва приймаючої команди, Team2 - назва гостьової команди, Goals - сума всіх голів за матч
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 8 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
-
 
         // Запит 9
         static void Query9(List<FootballGame> games)
@@ -177,14 +153,12 @@ namespace Practice_Linq_2024
             //Query 9: Перетворити всі матчі UEFA Nations League у 2023 році на матчі з наступними властивостями:
             // MatchYear - рік матчу, Game - назви обох команд через дефіс (першою - Home_team), Result - результат для першої команди (Win, Loss, Draw)
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 9 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
 
         // Запит 10
@@ -192,17 +166,12 @@ namespace Practice_Linq_2024
         {
             //Query 10: Вивести з 5-го по 10-тий (включно) матчі Gold Cup, які відбулися у липні 2023 р.
 
-            var selectedGames = games;    // Корегуємо запит !!!
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 10 ========================");
 
             // див. приклад як має бути виведено:
-
-
         }
-
-
-
     }
 }
